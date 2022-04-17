ee5d483099e95395e865ed49aec5a006f66f2d51
Iain Tierney <iaintier@buffalo.edu>
2022-04-16 23:31:23 -0400

    Developed contract further and added directory for front-end

--- contracts/Lane.sol
+++ contracts/Lane.sol
@@ -21,7 +21,7 @@ contract Lane {
     address private _owner;
     mapping(uint => Bike) public bikes;
     uint public bike_count = 0;
-    uint max_rental_days = 7;
+    uint public max_rental_days = 7;
 
 
     modifier onlyOwner() {
@@ -39,34 +39,17 @@ contract Lane {
     }
 
 
-
-    function createListing(uint deposit, uint fee) public {
-        // Require a valid deposit and fee
-        require(deposit > 0, "Deposit must be greater than zero.");
-        require(fee > 0, "Fee must be greater than zero.");
-
-        bike_count++;
-        
-        bikes[bike_count] = Bike(
-            bike_count,
-            deposit,
-            fee,
-            msg.sender,
-            msg.sender,
-            0,
-            0,
-            false,
-            false,
-            false
-        );
+    function viewBalance() public view returns(uint) {
+        return address(this).balance;
     }
+
 
     function rentBike(uint id, uint rental_days) public payable validBikeId(id) {
         Bike memory bike = bikes[id];
         address payable owner = bike.owner;
 
         // Require enough Ether, not rented, owner isn't trying to borrow their own bike, rental days is under max rental time
-        require(msg.value >= bike.deposit + bike.fee, "Not enough funds to rent!");
+        require(msg.value >= bike.deposit + (bike.fee * rental_days), "Not enough funds to rent!");
         require(!bike.rented, "Bike is already rented.");
         require(owner != msg.sender, "Listing owner cannot rent.");
         require(rental_days <= max_rental_days, "You cannot rent for more than the max rental days.");
@@ -74,51 +57,69 @@ contract Lane {
         // Refund if renter sent more than required
 		if (msg.value > bike.deposit + bike.fee) {
 			uint refund = msg.value - (bike.deposit + bike.fee);
-			msg.sender.transfer(refund);
+			payable(msg.sender).transfer(refund);
 		}
 
-        bike.renter = msg.sender;
+        bike.renter = payable(msg.sender);
         bike.rental_start = block.timestamp;
         bike.rented = true;
         bikes[id] = bike;
     }
 
-    function returnRentalOwner(uint id) public payable validBikeId(id) {
+    function returnRentalOwner(uint id) public validBikeId(id) {
         Bike memory bike = bikes[id];
         address payable owner = bike.owner;
         address payable renter = bike.renter;
 
-        // Require sender is the bike owner, bike is rented
+        // Require bike is rented, bike hasn't been returned by owner, sender is the owner
         require(bike.rented, "Bike has not been rented.");
+        require(!bike.owner_return, "You have already returned the bike.");
         require(msg.sender == owner, "Bike owner must initiate rental return!");
+
+        bike.owner_return = true;
+    }
+
+    function returnRentalRenter(uint id) public validBikeId(id){
+        Bike memory bike = bikes[id];
+        address payable owner = bike.owner;
+        address payable renter = bike.renter;
+
+        // Require bike is rented, bike hasn't been returned by renter, sender is the renter
+        require(bike.rented, "Bike has not been rented.");
+        require(!bike.renter_return, "You have already returned the bike.");
+        require(msg.sender == renter, "Bike renter must initiate rental return!");
+
+        bike.renter_return = true;
+    }
+
+    function endRental(uint id) public payable validBikeId(id) {
+        Bike memory bike = bikes[id];
+        address payable owner = bike.owner;
+        address payable renter = bike.renter;
+
+        // Require bike is rented, both renter and owner have confirmed bike return, sender is owner or renter
+        require(bike.rented, "Bike has not been rented.");
+        require(bike.owner_return && bike.renter_return, "Both the owner and the renter must verify bike return.");
+        require(msg.sender == owner || msg.sender == renter, "Bike owner or renter must end rental.");
 
         // Determine rental period, and associated rental cost
         uint end_time = block.timestamp;
-        uint rental_cost = rental_days;
-        rentalCost = _rentalDays * bike.rentalFee;
-        rentalCost = bike.rentalDeposit <= rentalCost
-            ? bike.rentalDeposit
-            : rentalCost; // x <= y ? x : y
-        // Transfer responsibility back to owner
-        bike.custodian = bike.owner;
-        // Mark as rented
-        bike.rented = false;
-        // Update the product
-        products[_id] = bike;
-        // Pay the owner by sending them Ether
-        address(_owner).transfer(rentalCost);
-        // Return remaining deposit to the borrower
-        uint256 returnedDeposit;
-        returnedDeposit = bike.rentalDeposit - rentalCost;
-        address(_borrower).transfer(returnedDeposit);
+        uint rental_cost = bike.rental_duration * bike.fee;
         
+        // Change bike state to not rented
+        bike.renter = bike.owner;
+        bike.rental_start = 0;
+        bike.rental_duration = 0;
+        bike.rented = false;
+        bike.owner_return = false;
+        bike.renter_return = false;
+        bikes[id] = bike;
+
+        // Pay owner and return deposit
+        owner.transfer(rental_cost);
+        renter.transfer(bike.deposit);
     }
 
-    function returnRentalRenter(uint id) public payable validBikeId(id){
-        Bike memory bike = bikes[id];
-        address payable owner = bike.owner;
-        address payable renter = bike.renter;
-    }
 
     function bikeStolen(uint id) public payable validBikeId(id) {
         Bike memory bike = bikes[id];
@@ -131,65 +132,96 @@ contract Lane {
         require(msg.sender == owner, "Only the bike owner can report a bike as stolen!");
         require(days_elapsed > bike.rental_duration + 1, "The bike must be rented for at least one day over the specified rental days to be reported as stolen!");
 
+        // Change bike state to not rented
+        bike.renter = bike.owner;
+        bike.rental_start = 0;
+        bike.rental_duration = 0;
+        bike.rented = false;
+        bike.owner_return = false;
+        bike.renter_return = false;
+        bikes[id] = bike;
+
         // Refund owner the rental deposit and fee
-        address(owner).transfer(bike.deposit + bike.fee);
+        owner.transfer(bike.deposit + bike.fee);
     }
 
-    function deleteProduct(uint id) public validBikeId(id) {
-        // Fetch the product
-        Product memory bike = products[_id];
-        // Make sure the product has a valid id
-        require(bike.id > 0 && bike.id <= productCount);
-        // Make sure only the product owner can delete a product
-        require(bike.owner == msg.sender);
-        // Make sure the owner is currently the custodian of the product
-        require(bike.custodian == bike.owner);
-        // Mark as rented (i.e. unavailable to rent)
-        bike.rented = true;
-        // Update the product
-        products[_id] = bike;
-        // Trigger an event
-        emit ProductDeleted(
-            productCount,
-            bike.name,
-            bike.owner,
-            bike.custodian,
-            bike.rented
-        );
-    }
 
-    function editProduct(
-        uint256 _id,
-        string memory _name,
-        string memory _description,
-        string memory _category,
-        uint256 _rentalDeposit,
-        uint256 _rentalFee
-    ) public {
-        // Fetch the product
-        Product memory bike = products[_id];
-        // Make sure the product has a valid id
-        require(bike.id > 0 && bike.id <= productCount);
-        // Make sure only the product owner can edit a product
-        require(bike.owner == msg.sender);
-        // Make sure the owner is currently the custodian of the product
-        require(bike.custodian == bike.owner);
-        // Edit the product
-        bike.name = _name;
-        bike.description = _description;
-        bike.category = _category;
-        bike.rentalDeposit = _rentalDeposit;
-        bike.rentalFee = _rentalFee;
-        products[_id] = bike;
-        emit ProductEdited(
-            productCount,
-            _name,
-            _description,
-            _category,
-            _rentalDeposit,
-            _rentalFee,
-            msg.sender,
+    function createListing(uint deposit, uint fee) public {
+        // Require a valid deposit and fee
+        require(deposit > 0, "Deposit must be greater than zero.");
+        require(fee > 0, "Fee must be greater than zero.");
+
+        bike_count++;
+        
+        bikes[bike_count] = Bike(
+            bike_count,
+            deposit,
+            fee,
+            payable(msg.sender),
+            payable(msg.sender),
+            0,
+            0,
+            false,
+            false,
             false
         );
     }
+
+    // function deleteProduct(uint id) public validBikeId(id) {
+    //     // Fetch the product
+    //     Product memory bike = products[_id];
+    //     // Make sure the product has a valid id
+    //     require(bike.id > 0 && bike.id <= productCount);
+    //     // Make sure only the product owner can delete a product
+    //     require(bike.owner == msg.sender);
+    //     // Make sure the owner is currently the custodian of the product
+    //     require(bike.custodian == bike.owner);
+    //     // Mark as rented (i.e. unavailable to rent)
+    //     bike.rented = true;
+    //     // Update the product
+    //     products[_id] = bike;
+    //     // Trigger an event
+    //     emit ProductDeleted(
+    //         productCount,
+    //         bike.name,
+    //         bike.owner,
+    //         bike.custodian,
+    //         bike.rented
+    //     );
+    // }
+
+    // function editProduct(
+    //     uint256 _id,
+    //     string memory _name,
+    //     string memory _description,
+    //     string memory _category,
+    //     uint256 _rentalDeposit,
+    //     uint256 _rentalFee
+    // ) public {
+    //     // Fetch the product
+    //     Product memory bike = products[_id];
+    //     // Make sure the product has a valid id
+    //     require(bike.id > 0 && bike.id <= productCount);
+    //     // Make sure only the product owner can edit a product
+    //     require(bike.owner == msg.sender);
+    //     // Make sure the owner is currently the custodian of the product
+    //     require(bike.custodian == bike.owner);
+    //     // Edit the product
+    //     bike.name = _name;
+    //     bike.description = _description;
+    //     bike.category = _category;
+    //     bike.rentalDeposit = _rentalDeposit;
+    //     bike.rentalFee = _rentalFee;
+    //     products[_id] = bike;
+    //     emit ProductEdited(
+    //         productCount,
+    //         _name,
+    //         _description,
+    //         _category,
+    //         _rentalDeposit,
+    //         _rentalFee,
+    //         msg.sender,
+    //         false
+    //     );
+    // }
 }
